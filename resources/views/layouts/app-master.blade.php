c3b9fccc4f88af0650563896a01b593eae61cd44
RahulRawatAgt <rahulrawat.agt@gmail.com>
2024-01-19 18:26:15 +0530

    added the js and css localize

--- resources/views/layouts/app-master.blade.php
+++ resources/views/layouts/app-master.blade.php
@@ -12,8 +12,8 @@
   <title>Ajanta | Brivex Eye Drops</title>
   <!-- css file -->
   <!-- DataTables CSS and JS -->
-<link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.13.7/css/jquery.dataTables.min.css">
-<link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/buttons/2.4.2/css/buttons.dataTables.min.css">
+<link rel="stylesheet" type="text/css" href="{{asset('theme/css/jquery.dataTables.min.css')}}">
+<link rel="stylesheet" type="text/css" href="{{asset('theme/css/buttons.dataTables.min.css')}}">
 
 
   <link rel="stylesheet" href="{{asset('theme/css/bootstrap.min.css')}}">
@@ -123,14 +123,14 @@
   <script src="{{asset('theme/js/jquery-scrolltofixed-min.js')}}"></script>
   <script src="{{asset('theme/js/jquery.counterup.js')}}"></script>
   <script src="{{asset('theme/js/wow.min.js')}}"></script>
-  <script type="text/javascript" charset="utf8" src="https://cdn.datatables.net/1.13.7/js/jquery.dataTables.min.js"></script>
+  <script type="text/javascript" charset="utf8" src="{{asset('theme/js/jquery.dataTables.min.js')}}"></script>
   <!-- DataTables Buttons CSS and JS -->
-  <script type="text/javascript" charset="utf8" src="https://cdn.datatables.net/buttons/2.4.2/js/dataTables.buttons.min.js"></script>
-  <script type="text/javascript" charset="utf8" src="https://cdnjs.cloudflare.com/ajax/libs/jszip/3.10.1/jszip.min.js"></script>
-  <script type="text/javascript" charset="utf8" src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.53/pdfmake.min.js"></script>
-  <script type="text/javascript" charset="utf8" src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.53/vfs_fonts.js"></script>
-  <script type="text/javascript" charset="utf8" src="https://cdn.datatables.net/buttons/2.4.2/js/buttons.html5.min.js"></script>
-  <script type="text/javascript" charset="utf8" src="https://cdn.datatables.net/buttons/2.4.2/js/buttons.print.min.js"></script>
+  <script type="text/javascript" charset="utf8" src="{{asset('theme/js/dataTables.buttons.min.js')}}"></script>
+  <script type="text/javascript" charset="utf8" src="{{asset('theme/js/jszip.min.js')}}"></script>
+  <script type="text/javascript" charset="utf8" src="{{asset('theme/js/pdfmake.min.js')}}"></script>
+  <script type="text/javascript" charset="utf8" src="{{asset('theme/js/vfs_fonts.js')}}"></script>
+  <script type="text/javascript" charset="utf8" src="{{asset('theme/js/buttons.html5.min.js')}}"></script>
+  <script type="text/javascript" charset="utf8" src="{{asset('theme/js/buttons.print.min.js')}}"></script>
 
   <script src="{{asset('theme/js/progressbar.js')}}"></script>
   <script src="{{asset('theme/js/slider.js')}}"></script>
